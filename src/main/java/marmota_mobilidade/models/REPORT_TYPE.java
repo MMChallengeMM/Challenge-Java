944d85db5db2ca5120660d503b685d86020de968
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-02-20 12:02:14 -0300

    atualizando relatório

---
+++ src/main/java/marmota_mobilidade/models/REPORT_TYPE.java
@@ -0,0 +1,7 @@
+package marmota_mobilidade.models;
+
+public enum REPORT_TYPE {
+    GERAL,
+    PERIODO,
+    TIPO_DE_FALHA
+}
--- src/main/java/marmota_mobilidade/models/Report.java
+++ src/main/java/marmota_mobilidade/models/Report.java
@@ -1,4 +1,23 @@
 package marmota_mobilidade.models;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
 public class Report extends _BaseEntity {
+    private REPORT_TYPE reportType;
+    private LocalDateTime generationDate;
+    private int numberOfFailures;
+    private String reportData;
+    private Failure[] lastFailures = new Failure[5];
+
 }
